e6cf7a1dc1d5a7c342eca45f1c2f3f2c2932e617
Igor Nicoletti <igornicoletti@outlook.com>
2022-07-12 00:56:35 -0300

    theme mode title

--- src/components/ThemeMode/index.tsx
+++ src/components/ThemeMode/index.tsx
@@ -4,20 +4,19 @@ import { useTheme } from '../../hooks/useTheme'
 
 export function ThemeMode() {
   const { isThemeMode, isSetThemeMode } = useTheme()
-  const handleToggleTheme = () => isSetThemeMode(!isThemeMode)
 
   return (
     <>
       <button
         type="button"
-        title="ThemeMode"
-        onClick={handleToggleTheme}
+        onClick={() => isSetThemeMode(!isThemeMode)}
+        title={`${isThemeMode ? "Dark Mode" : "Light Mode"}`}
         className="fixed top-0 right-0 p-5
         text-dark/50 dark:text-white/50 
         hover:text-dark dark:hover:text-white 
-        focus-visible:text-dark dark:focus-visible:text-white
-        transition ease-in-out duration-250
-        focus:outline-none focus-visible:outline-none">
+        focus-visible:text-dark dark:focus-visible:text-white        
+        focus:outline-none focus-visible:outline-none
+        transition ease-in-out duration-250">
         {isThemeMode ? (<Moon size={20} />) : (<SunDim size={20} />)}
       </button>
     </>
