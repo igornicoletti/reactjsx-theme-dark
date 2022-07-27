8c63b5ecb6ef6fb4aa4412abf1f62f1318ad571a
Igor Nicoletti <igornicoletti@outlook.com>
2022-07-27 05:25:11 -0300

    first commit

--- src/components/ThemeMode/index.tsx
+++ src/components/ThemeMode/index.tsx
@@ -12,12 +12,12 @@ export function ThemeMode() {
         onClick={() => isSetThemeMode(!isThemeMode)}
         title={`${isThemeMode ? "Dark Mode" : "Light Mode"}`}
         className="fixed top-0 right-0 p-5
-        text-dark/50 dark:text-white/50 
+        text-dark/75 dark:text-white/75
         hover:text-dark dark:hover:text-white 
         focus-visible:text-dark dark:focus-visible:text-white        
         focus:outline-none focus-visible:outline-none
         transition ease-in-out duration-250">
-        {isThemeMode ? (<Moon size={20} />) : (<SunDim size={20} />)}
+        {isThemeMode ? (<Moon size={24} />) : (<SunDim size={24} />)}
       </button>
     </>
   )
